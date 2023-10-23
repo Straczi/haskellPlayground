96cb18a8e1079617168f261555941b21cbd92a4b
Straczi <k.arendt0302@gmail.com>
2023-10-23 23:34:25 +0200

    solve Serie5 exercise

---
+++ Uni-Aufgaben/Serie5.hs
@@ -0,0 +1,26 @@
+isMonotonous :: Ord a => [a] -> Bool
+isMonotonous [] = True
+isMonotonous [a] = True
+isMonotonous l = ( (l!!0 <= l!!1)  && isMonotonousDec (tail l) ) || ( (l!!0 >= l!!1)  && isMonotonousInc (tail l) )
+                where isMonotonousDec ls = length ls <2 || (ls!!0 <= ls!!1)  && isMonotonousDec (tail ls)
+                      isMonotonousInc ls = length ls <2 || (ls!!0 >= ls!!1)  && isMonotonousInc (tail ls)
+
+monotoneFolge :: [Integer]
+monotoneFolge = [1,2,3,4,5]
+
+
+isAlternating :: (Ord a, Num a) => [a] -> Bool
+isAlternating [] = False
+isAlternating [a] = False
+isAlternating l = (l!!0 * l!!1 < 0) && ( length l == 2 || isAlternating (tail l) )
+
+alternierendeFolge :: [Integer]
+alternierendeFolge = [-1, 1, -2, 2, -3, 3]
+
+isConstant :: Eq a => [a] -> Bool
+isConstant [] = True
+isConstant [a] = True
+isConstant l = (l!!0 == l!!1) && isConstant(tail l)
+
+konstanteFolge :: [Integer]
+konstanteFolge = [1, 1, 1, 1]
